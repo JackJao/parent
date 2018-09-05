d11cf2d0fbde98b3f728ec1e4ee99fd03b47a4d4
JackJao <766056142@qq.com>
2018-09-05 11:31:46 +0800

    调整pom文件，增加注释

--- service-client1/src/main/java/com/springclound/serviceclient1/ServiceClient1Application.java
+++ service-client1/src/main/java/com/springclound/serviceclient1/ServiceClient1Application.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 1.当client向server注册时，它会提供一些元数据，例如主机和端口，URL，主页等。Eureka server 从每个client实例接收心跳消息。 如果心跳超时，则通常将该实例从注册server中删除。
+ * 2.通过注解@EnableEurekaClient 表明自己是一个eurekaclient.
+ * 3.@RestController开放一个restAPI入口
+ */
 @EnableEurekaClient
 @SpringBootApplication
 @RestController
